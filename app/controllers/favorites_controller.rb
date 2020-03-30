ae8f028a76df3ba9c3fe678aa7ebe1265bd72743
taishi352 <taishi352@gmail.com>
2020-03-30 07:30:59 +0000

    [update]いいね機能、コメント機能の非同期化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,12 +3,22 @@ class FavoritesController < ApplicationController
 		@post = Post.find(params[:post_id])
 		@favorite = current_user.favorites.new(post_id: @post.id)
 		@favorite.save
-		redirect_back(fallback_location: root_path)
+		# redirect_back(fallback_location: root_path)
 	end
 	def destroy
 		@post = Post.find(params[:post_id])
 		@favorite = current_user.favorites.find_by(post_id: @post.id)
 		@favorite.destroy
-		redirect_back(fallback_location: root_path)
+		# redirect_back(fallback_location: root_path)
 	end
+
+	private
+  def redirect
+    case params[:redirect_id].to_i
+    when 0
+      redirect_to posts_path
+    when 1
+      redirect_to post_path(@post)
+    end
+  end
 end
--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -4,14 +4,14 @@ class PostCommentsController < ApplicationController
 		@comment = current_user.post_comments.new(post_comment_params)
 		@comment.post_id = @post.id
 		@comment.save
-		redirect_back(fallback_location: root_path)
+		# redirect_back(fallback_location: root_path)
 	end
 	def destroy
 		@post = Post.find(params[:post_id])
 		@comment = current_user.post_comments.find_by(post_id: @post.id)
 		@comment.post_id = @post.id
 		@comment.destroy
-		redirect_back(fallback_location: root_path)
+		# redirect_back(fallback_location: root_path)
 	end
 
 	private
